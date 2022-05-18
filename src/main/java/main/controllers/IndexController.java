f8464f89803d9544ba6c48114f90037644fe49aa
Alex Melnikov <amelnikov@abisoft.spb.ru>
2022-05-19 01:26:14 +0300

    Прикручен фронт

--- src/main/java/main/controllers/IndexController.java
+++ src/main/java/main/controllers/IndexController.java
@@ -1,12 +1,24 @@
 package main.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/")
+@Slf4j
 public class IndexController {
+
+    @GetMapping("/")
     public String index() {
-        return "index";
+        log.info("Request: /index");
+        return "index.html";
+    }
+
+    @RequestMapping("/ping")
+    @ResponseBody
+    public String ping(){
+        return "ping";
     }
 }
